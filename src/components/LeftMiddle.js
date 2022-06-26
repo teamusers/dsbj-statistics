21e5ae61138229c824d339c5424850969c4676e4
Stephen Cheng <anh.trinh@user.com.sg>
2022-06-27 06:37:59 +0700

    feat: complete right block

--- src/components/LeftMiddle.js
+++ src/components/LeftMiddle.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Box, Stack } from "@mui/material";
 
 import Card from "./common/Card";
-import Circle from "./common/Circle";
+import Circular from "./common/Circular";
 
 const LeftMiddle = (props) => (
   <Box {...props}>
     <Card size="small" title="达成率">
       <Stack direction="row" justifyContent="space-around" marginTop={2} px={2}>
-        <Circle color="red" value="78%" label="一线人均产值" />
-        <Circle color="yellow" value="82%" label="二线人均产值" />
-        <Circle color="blue" value="95%" label="一线人均销售额" />
-        <Circle color="purple" value="101%" label="二线人均销售额" />
+        <Circular color="red" value={78} label="一线人均产值" />
+        <Circular color="yellow" value={82} label="二线人均产值" />
+        <Circular color="blue" value={95} label="一线人均销售额" />
+        <Circular color="purple" value={101} label="二线人均销售额" />
       </Stack>
     </Card>
   </Box>
--- src/components/common/Circle/index.js
+++ src/components/common/Circle/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Box } from "@mui/material";
-import cn from "classnames";
 
 import styles from "./index.module.scss";
 
 const Circle = ({ color, value, description, label }) => {
   return (
-    <Box className={cn(styles.container, { [styles[color]]: color })}>
-      <Box className={cn(styles.circle)}>
+    <Box className={styles.container}>
+      <Box className={styles.circle}>
         <Box className={styles.value}>{value}</Box>
         <Box className={styles.description}>{description}</Box>
       </Box>
