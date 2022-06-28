31cd266840d4444e428cd998b9731d62b2295c15
John Darnel Velasquez <darnel.ve@user.com.sg>
2022-06-28 13:06:21 +0800

    Add color when hover or select in small dropdown in left bottom view.

--- src/components/common/SelectDropdown/index.js
+++ src/components/common/SelectDropdown/index.js
@@ -7,9 +7,20 @@ import cn from "classnames";
 import styles from "./index.module.scss";
 
 const SelectDropdown = ({ position = "" }) => {
-  const [dropdownFirst, setDropdownFirst] = React.useState(1);
-  const [dropdownSecond, setDropdownSecond] = React.useState(1);
-  const [dropdownThird, setDropdownThird] = React.useState(1);
+  const [dropdownFirst, setDropdownFirst] = React.useState(0);
+  const [dropdownSecond, setDropdownSecond] = React.useState(0);
+  const [dropdownThird, setDropdownThird] = React.useState(0);
+
+  const menuItems = ["全部", "集团", "维信", "超毅", "LED", "触控", "精密"];
+  const menuItemsClassNames = [
+    "menu-0",
+    "menu-1",
+    "menu-2",
+    "menu-3",
+    "menu-4",
+    "menu-5",
+    "menu-6",
+  ];
 
   const handleDropdownFirstChange = (event) => {
     setDropdownFirst(event.target.value);
@@ -39,14 +50,18 @@ const SelectDropdown = ({ position = "" }) => {
               onChange={handleDropdownFirstChange}
               select
               SelectProps={{
-                classes: { icon: styles.selectIcon },
+                classes: {
+                  icon: cn(styles.selectIcon, {
+                    [styles[menuItemsClassNames[dropdownFirst]]]:
+                      menuItemsClassNames[dropdownFirst],
+                  }),
+                },
                 IconComponent: (defaultProps) => (
                   <KeyboardArrowDownIcon {...defaultProps} />
                 ),
                 MenuProps: {
                   classes: {
                     root: styles.menuRoot,
-                    selected: styles.menuSelected,
                   },
                 },
               }}
@@ -54,18 +69,32 @@ const SelectDropdown = ({ position = "" }) => {
                 classes: {
                   root: cn(styles.inputRoot, {
                     [styles["small"]]: "small",
+                    [styles[menuItemsClassNames[dropdownFirst]]]:
+                      menuItemsClassNames[dropdownFirst],
                   }),
                   notchedOutline: styles.inputNotchedOutline,
                 },
               }}
             >
-              <MenuItem value={1}>● 全部</MenuItem>
-              <MenuItem value={2}>● 集团</MenuItem>
-              <MenuItem value={3}>● 维信</MenuItem>
-              <MenuItem value={4}>● 超毅</MenuItem>
-              <MenuItem value={5}>● LED</MenuItem>
-              <MenuItem value={6}>● 触控</MenuItem>
-              <MenuItem value={7}>● 精密</MenuItem>
+              {menuItems.map((menuItem, index) => {
+                return (
+                  <MenuItem
+                    classes={{
+                      root: cn(styles.menuItem, {
+                        [styles[menuItemsClassNames[index]]]:
+                          menuItemsClassNames[index],
+                      }),
+                      selected: cn(styles.menuSelected, {
+                        [styles[menuItemsClassNames[dropdownFirst]]]:
+                          menuItemsClassNames[dropdownFirst],
+                      }),
+                    }}
+                    value={index}
+                  >
+                    ● {menuItem}
+                  </MenuItem>
+                );
+              })}
             </TextField>
           </Grid>
           <Grid item>
@@ -88,10 +117,10 @@ const SelectDropdown = ({ position = "" }) => {
                 },
               }}
             >
-              <MenuItem value={1}>一线人均产值及达成率</MenuItem>
-              <MenuItem value={2}>二线人均产值及达成率</MenuItem>
-              <MenuItem value={3}>一线人均销售额及达成率</MenuItem>
-              <MenuItem value={4}>二线人均销售额及达成率</MenuItem>
+              <MenuItem value={0}>一线人均产值及达成率</MenuItem>
+              <MenuItem value={1}>二线人均产值及达成率</MenuItem>
+              <MenuItem value={2}>一线人均销售额及达成率</MenuItem>
+              <MenuItem value={3}>二线人均销售额及达成率</MenuItem>
             </TextField>
           </Grid>
         </>
@@ -117,11 +146,11 @@ const SelectDropdown = ({ position = "" }) => {
               },
             }}
           >
+            <MenuItem value={0}>一线人均产值</MenuItem>
             <MenuItem value={1}>一线人均产值</MenuItem>
-            <MenuItem value={2}>一线人均产值</MenuItem>
-            <MenuItem value={3}>二线人均产值</MenuItem>
-            <MenuItem value={4}>一线人均销售额</MenuItem>
-            <MenuItem value={5}>二线人均销售额</MenuItem>
+            <MenuItem value={2}>二线人均产值</MenuItem>
+            <MenuItem value={3}>一线人均销售额</MenuItem>
+            <MenuItem value={4}>二线人均销售额</MenuItem>
           </TextField>
         </Grid>
       )}
